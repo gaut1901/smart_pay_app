24191cb1fa0f5099328b33159bcd2a8ea7016232
bala <bala@avelator.com>
2026-02-16 20:24:11 +0530

    profile api fixed

--- lib/data/services/profile_service.dart
+++ lib/data/services/profile_service.dart
@@ -154,6 +154,258 @@ class ProfileService {
     }
   }
 
+  Future<Map<String, dynamic>> uploadFamilyPhoto({
+    required String empCode,
+    required String slno,
+    required String name,
+    required String relation,
+    required String bloodGroup,
+    required String mobileNo,
+    required String dob,
+    required String adharNo,
+    required String eduDetail,
+    required String occDetail,
+    File? photoFile,
+  }) async {
+    final user = AuthService.currentUser;
+    if (user == null) throw Exception('User not logged in');
+
+    final url = Uri.parse('${ApiConfig.baseUrl}${ApiConfig.familyPhotoUpload}/');
+    var request = http.MultipartRequest('POST', url);
+    
+    final headers = user.toHeaders();
+    headers.forEach((key, value) {
+      if (key != 'Content-Type') {
+        request.headers[key] = value;
+      }
+    });
+
+    request.fields['empcode'] = empCode;
+    request.fields['slno'] = slno;
+    request.fields['name'] = name;
+    request.fields['relation'] = relation;
+    request.fields['bloodgroup'] = bloodGroup;
+    request.fields['mobileno'] = mobileNo;
+    request.fields['dob'] = dob;
+    request.fields['adharno'] = adharNo;
+    request.fields['EduDetail'] = eduDetail;
+    request.fields['OccDetail'] = occDetail;
+    request.fields['filepath'] = 'undefined'; // Will be set by server
+
+    if (photoFile != null) {
+      final fileName = photoFile.path.split('/').last;
+      var multipartFile = await http.MultipartFile.fromPath(
+        fileName,
+        photoFile.path,
+        filename: fileName,
+      );
+      request.files.add(multipartFile);
+    }
+
+    try {
+      final streamedResponse = await request.send();
+      final response = await http.Response.fromStream(streamedResponse);
+
+      if (response.statusCode == 200) {
+        final data = jsonDecode(response.body);
+        return jsonDecode(data['response']);
+      } else {
+        throw Exception('Failed to upload family photo: ${response.statusCode}');
+      }
+    } catch (e) {
+      rethrow;
+    }
+  }
+
+  Future<Map<String, dynamic>> uploadEduPhoto({
+    required String empCode,
+    required String slno,
+    required String degreeType,
+    required String degree,
+    required String institution,
+    required String subject,
+    required String passDate,
+    File? eduFile,
+  }) async {
+    final user = AuthService.currentUser;
+    if (user == null) throw Exception('User not logged in');
+
+    final url = Uri.parse('${ApiConfig.baseUrl}${ApiConfig.eduPhotoUpload}/');
+    var request = http.MultipartRequest('POST', url);
+    
+    final headers = user.toHeaders();
+    headers.forEach((key, value) {
+      if (key != 'Content-Type') {
+        request.headers[key] = value;
+      }
+    });
+
+    request.fields['empcode'] = empCode;
+    request.fields['slno'] = slno;
+    request.fields['degreetype'] = degreeType;
+    request.fields['degree'] = degree;
+    request.fields['institution'] = institution;
+    request.fields['subject'] = subject;
+    request.fields['passdate'] = passDate;
+    request.fields['passyear'] = 'undefined'; // Not needed
+    request.fields['edufilepath'] = 'undefined'; // Will be set by server
+
+    if (eduFile != null) {
+      final fileName = eduFile.path.split('/').last;
+      var multipartFile = await http.MultipartFile.fromPath(
+        fileName,
+        eduFile.path,
+        filename: fileName,
+      );
+      request.files.add(multipartFile);
+    }
+
+    try {
+      final streamedResponse = await request.send();
+      final response = await http.Response.fromStream(streamedResponse);
+
+      if (response.statusCode == 200) {
+        final data = jsonDecode(response.body);
+        return jsonDecode(data['response']);
+      } else {
+        throw Exception('Failed to upload education photo: ${response.statusCode}');
+      }
+    } catch (e) {
+      rethrow;
+    }
+  }
+
+  Future<Map<String, dynamic>> uploadExpPhoto({
+    required String empCode,
+    required String slno,
+    required String companyName,
+    required String role,
+    required String duration,
+    required String expFrom,
+    required String expTo,
+    File? expFile,
+    File? relFile,
+  }) async {
+    final user = AuthService.currentUser;
+    if (user == null) throw Exception('User not logged in');
+
+    final url = Uri.parse('${ApiConfig.baseUrl}${ApiConfig.expPhotoUpload}/');
+    var request = http.MultipartRequest('POST', url);
+    
+    final headers = user.toHeaders();
+    headers.forEach((key, value) {
+      if (key != 'Content-Type') {
+        request.headers[key] = value;
+      }
+    });
+
+    request.fields['empcode'] = empCode;
+    request.fields['slno'] = slno;
+    request.fields['companyname'] = companyName;
+    request.fields['role'] = role;
+    request.fields['duration'] = duration;
+    request.fields['ExpFrom'] = expFrom;
+    request.fields['ExpTo'] = expTo;
+    request.fields['expfilepath'] = 'undefined'; // Will be set by server
+
+    if (expFile != null) {
+      final fileName = expFile.path.split('/').last;
+      var multipartFile = await http.MultipartFile.fromPath(
+        'Exp_$fileName',
+        expFile.path,
+        filename: 'Exp_$fileName',
+      );
+      request.files.add(multipartFile);
+    }
+
+    if (relFile != null) {
+      final fileName = relFile.path.split('/').last;
+      var multipartFile = await http.MultipartFile.fromPath(
+        'Rel_$fileName',
+        relFile.path,
+        filename: 'Rel_$fileName',
+      );
+      request.files.add(multipartFile);
+    }
+
+    try {
+      final streamedResponse = await request.send();
+      final response = await http.Response.fromStream(streamedResponse);
+
+      if (response.statusCode == 200) {
+        final data = jsonDecode(response.body);
+        return jsonDecode(data['response']);
+      } else {
+        throw Exception('Failed to upload experience photo: ${response.statusCode}');
+      }
+    } catch (e) {
+      rethrow;
+    }
+  }
+
+  Future<Map<String, dynamic>> uploadInsPhoto({
+    required String empCode,
+    required String slno,
+    required String insType,
+    required String insName,
+    required String insCmpName,
+    required String insNo,
+    required String insFor,
+    required String insAmount,
+    required String startDate,
+    required String endDate,
+    File? insFile,
+  }) async {
+    final user = AuthService.currentUser;
+    if (user == null) throw Exception('User not logged in');
+
+    final url = Uri.parse('${ApiConfig.baseUrl}${ApiConfig.insPhotoUpload}/');
+    var request = http.MultipartRequest('POST', url);
+    
+    final headers = user.toHeaders();
+    headers.forEach((key, value) {
+      if (key != 'Content-Type') {
+        request.headers[key] = value;
+      }
+    });
+
+    request.fields['EmpCode'] = empCode;
+    request.fields['SlNo'] = slno;
+    request.fields['InsType'] = insType;
+    request.fields['InsName'] = insName;
+    request.fields['InsCmpName'] = insCmpName;
+    request.fields['InsNo'] = insNo;
+    request.fields['InsFor'] = insFor;
+    request.fields['InsAmount'] = insAmount;
+    request.fields['StartDate'] = startDate;
+    request.fields['EndDate'] = endDate;
+    request.fields['FilePath'] = 'undefined'; // Will be set by server
+
+    if (insFile != null) {
+      final fileName = insFile.path.split('/').last;
+      var multipartFile = await http.MultipartFile.fromPath(
+        fileName,
+        insFile.path,
+        filename: fileName,
+      );
+      request.files.add(multipartFile);
+    }
+
+    try {
+      final streamedResponse = await request.send();
+      final response = await http.Response.fromStream(streamedResponse);
+
+      if (response.statusCode == 200) {
+        final data = jsonDecode(response.body);
+        return jsonDecode(data['response']);
+      } else {
+        throw Exception('Failed to upload insurance photo: ${response.statusCode}');
+      }
+    } catch (e) {
+      rethrow;
+    }
+  }
+
   Future<WagesModel> getWagesData() async {
     final user = AuthService.currentUser;
     if (user == null) throw Exception('User not logged in');
